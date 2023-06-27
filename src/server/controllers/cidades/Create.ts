5b0c527a1e7b54a7c75b6a350bcfc9b608c563fa
marceonesgenesis <marceones@genesistech.com.br>
2023-06-27 17:13:40 -0300

    adicionando bibliotece yup para validar schema de dados

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,13 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Request, Response } from 'express';
+import * as yup from 'yup';
 
 interface ICidade{
     nome: string;
 }
 
+const bodyValidation:yup.Schema<ICidade> = yup.object().shape({
+    nome:yup.string().required().min(3),
+});
 // 
-export const create = (req: Request<{},{},ICidade>,res: Response)=>{
-     
-    const data = req.body;
-    console.log(data);
+export const create = async (req: Request<{},{},ICidade>,res: Response)=>{
+ let validateData: ICidade | undefined = undefined;
+ console.log(req.body);
+    try{
+        validateData = await bodyValidation.validate(req.body);
+        console.log(req.body);
+    }catch(error){
+        const yupError = error as yup.ValidationError;
+        return res.json({
+            errors:{
+                default: yupError.message,
+            }
+        });
+        console.log(validateData);
+  }
+
     return res.send('Create');
 };
