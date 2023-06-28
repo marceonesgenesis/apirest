7f5d11900f18278b2d2d1dbc8c33b98f1b3d8778
marceonesgenesis <marceones@genesistech.com.br>
2023-06-28 07:46:22 -0300

    refactor: validaçaõ de campos enviados pelo usuario atraves da classe de erros

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -4,27 +4,32 @@ import * as yup from 'yup';
 
 interface ICidade{
     nome: string;
+    estado:string;
 }
 
 const bodyValidation:yup.Schema<ICidade> = yup.object().shape({
     nome:yup.string().required().min(3),
+    estado:yup.string().required().min(3),
 });
 // 
 export const create = async (req: Request<{},{},ICidade>,res: Response)=>{
- let validateData: ICidade | undefined = undefined;
- console.log(req.body);
+    let validateData: ICidade | undefined = undefined;
+   
     try{
-        validateData = await bodyValidation.validate(req.body);
-        console.log(req.body);
+        validateData = await bodyValidation.validate(req.body,{abortEarly:false});
+       
     }catch(error){
         const yupError = error as yup.ValidationError;
-        return res.json({
-            errors:{
-                default: yupError.message,
-            }
+        const validationErrors: Record<string,string> = {};
+
+        yupError.inner.forEach(error =>{
+            if(error.path === undefined) return;
+            validationErrors[error.path] = error.message;
         });
+
+        return res.status(400).json({errors:validationErrors});
         console.log(validateData);
-  }
+    }
 
     return res.send('Create');
 };
